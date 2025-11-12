ee85bab1567ca843d9fa03ea77876567f6b08a0e
yongklit33 <s43495@bj.ac.th>
2025-11-12 11:06:06 +0700

    Add files via upload

---
+++ a1_platform_33/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1762920291,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/coinbox-sheet0.png",
+		"images/coin-sheet0.png",
+		"images/player-sheet0.png",
+		"images/tiledbackground.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
