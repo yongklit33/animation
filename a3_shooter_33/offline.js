ea4f3b6ed2aa2c7c6ef3cc54c93659088cf88cee
yongklit33 <s43495@bj.ac.th>
2026-01-07 11:06:00 +0700

    Add files via upload

---
+++ a3_shooter_33/offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1767758791,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/background.png",
+		"images/player-sheet0.png",
+		"images/monster-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/explosion-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
