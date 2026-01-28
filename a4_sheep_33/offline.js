241f6ea02de99c9495786b0e0116e2e4eedb4ba5
yongklit33 <s43495@bj.ac.th>
2026-01-28 11:28:37 +0700

    Add files via upload

---
+++ a4_sheep_33/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1769574640,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/bg-sheet0.png",
+		"images/land-sheet0.png",
+		"images/land-sheet1.png",
+		"images/tiledbackground.png",
+		"images/line.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
